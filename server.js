b5d4a9e65fabd04b9b7e95f632dd057a96516949
tomluehewshb <tomluehe.wshb@gmail.com>
2026-05-06 19:06:47 +0200

    server.js

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const fs = require("fs");
+const cors = require("cors");
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 
 app.post("/speichern", (req, res) => {
